0c06f2f784f8f8470634b5336819164d0949a508
emrsyhn <emirseyhana.z@gmail.com>
2023-06-25 17:11:22 +0300

    firebase kaldırıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 import 'binding/auth_bindings.dart';
-import 'firebase_options.dart';
 import 'router/route_manager.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
